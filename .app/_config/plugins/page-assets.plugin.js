818f570cb0a5001574610b17bb39eb847f14508b
uncenter <me@uncenter.org>
2023-04-23 17:02:13 -0400

    fix: match *.svg assets with eleventy-plugin-page-assets

--- .app/_config/plugins/page-assets.plugin.js
+++ .app/_config/plugins/page-assets.plugin.js
@@ -1,4 +1,4 @@
 module.exports = [
 	require('eleventy-plugin-page-assets'),
-	{ mode: 'parse', postsMatching: '*.md' },
+	{ mode: 'parse', postsMatching: '*.md', assetsMatching: '*.png|*.jpg|*.svg|*.gif' },
 ];
